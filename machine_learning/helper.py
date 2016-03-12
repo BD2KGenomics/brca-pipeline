6f34c3e589649ef9d713859c8f0fa87e6af9bf8b
Molly Zhang <mollyzhang@ucsc.edu>
2016-03-11 21:20:40 -0800

    add helper function

---
+++ machine_learning/helper.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def folds_to_split(data,targets,train,test):
+    data_tr = pd.DataFrame(data).iloc[train]
+    data_te = pd.DataFrame(data).iloc[test]
+    labels_tr = pd.DataFrame(targets).iloc[train]
+    labels_te = pd.DataFrame(targets).iloc[test]
+    return [data_tr, data_te, labels_tr, labels_te]
