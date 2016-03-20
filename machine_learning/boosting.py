4de79f9289f9310f4d454412ed227141227d7819
Molly Zhang <mollyzhang@ucsc.edu>
2016-03-20 01:10:02 -0700

    add boosting.py for my personal implementation of adaboost and LPboost

---
+++ machine_learning/boosting.py
@@ -0,0 +1,13 @@
+""" implementing Adaboost and LPBoost based on
+Manfred Warmuth's video lecture and boosting tutorial which can be found here:
+Video: http://videolectures.net/icml09_warmuth_vishwanathan_sbop/
+Slide: https://users.soe.ucsc.edu/~manfred/pubs/tut/purdue2011.pdf
+"""
+
+class Adaboost():
+    pass
+
+
+
+class LPboost():
+    pass
