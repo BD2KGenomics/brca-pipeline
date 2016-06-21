8adc5d911eb43ea486cd7cb5928adfe1a076d526
Molly Zhang <mollyzhang@ucsc.edu>
2016-06-21 14:36:03 -0700

    update fetching brca sequence from fasta script

--- resources/fetch_brca12_from_hg.py
+++ resources/fetch_brca12_from_hg.py
@@ -1,14 +1,13 @@
 import pysam
-chr13_fa = "/cluster/home/mollyzhang/chr13.fa"
-chr17_fa = "/cluster/home/mollyzhang/chr17.fa"
-brca1 = "/cluster/home/mollyzhang/brca_repo/resources/brca1_hg38.txt"
-brca2 = "/cluster/home/mollyzhang/brca_repo/resources/brca2_hg38.txt"
-
+chr13_fa = "/cluster/home/mollyzhang/reference_genome/hg19/chr13.fa"
+chr17_fa = "/cluster/home/mollyzhang/reference_genome/hg19/chr17.fa"
+brca1 = "/cluster/home/mollyzhang/brca_repo/resources/brca1_hg19_no_flanking.txt"
+brca2 = "/cluster/home/mollyzhang/brca-pipeline/resources/brca2_hg19_no_flanking.txt"
 
 chr13 = pysam.FastaFile(chr13_fa)
 chr17 = pysam.FastaFile(chr17_fa)
-BRCA2 = chr13.fetch(reference="chr13", start=32300000, end=32500000)
-BRCA1 = chr17.fetch(reference="chr17", start=43000000, end=43200000)
+BRCA2 = chr13.fetch(reference="chr13", start=32889617, end=32973809)
+BRCA1 = chr17.fetch(reference="chr17", start=41196312, end=41277500)
 
 open(brca1, "w").write(BRCA1)
 open(brca2, "w").write(BRCA2)
