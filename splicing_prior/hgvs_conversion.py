272f8b1401b15b10c3f5089c633697ddc440c1fa
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-12 10:47:48 -0700

    add vcf to hgvs conversion function

--- splicing_prior/hgvs_conversion.py
+++ splicing_prior/hgvs_conversion.py
@@ -11,11 +11,20 @@ from pygr.seqdb import SequenceFileDB
 HG38 = SequenceFileDB("/cluster/home/mollyzhang/reference_genome/hg38/hg38.fa")
 
 with open('../resources/refseq/hg38.BRCA.refGene.txt') as infile:
-    transcripts = pyhgvs_utils.read_transcripts(infile)
+    transcripts = hgvs_utils.read_transcripts(infile)
 
 def get_transcript(name):
     return transcripts.get(name)
 
 def VCF_to_HGVS(genome_coor):
-    pass
+    chrom, pos, refalt = genome_coor.replace("-", "").split(":")
+    ref, alt = refalt.split(">")
+    if chrom == "chr13":
+        transcript = get_transcript("NM_000059")
+    elif chrom == "chr17":
+        transcript = get_trascript("NM_007294")
+    else:
+        raise Exception("chromsome name error")
+    hgvs_name = pyhgvs.format_hgvs_name(chrom, pos, ref, alt, genome, transcript)
+    return hgvs_name
 
