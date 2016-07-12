553233579588457fa92f2cf7cefa56f9715bfb42
Molly Zhang <mollyzhang@ucsc.edu>
2016-07-12 10:20:20 -0700

    add refseq importing script

--- splicing_prior/hgvs_conversion.py
+++ splicing_prior/hgvs_conversion.py
@@ -1,8 +1,21 @@
+"""
+convert VCF genomic coordinate format to HGVS format and back
+Note: currently only supporting genomic assembly version HG38 (GRCh38)
+"""
+
 import pyhgvs
 import pyhgvs.utils as hgvs_utils
 from pygr.seqdb import SequenceFileDB
 
+
 HG38 = SequenceFileDB("/cluster/home/mollyzhang/reference_genome/hg38/hg38.fa")
 
+with open('../resources/refseq/hg38.BRCA.refGene.txt') as infile:
+    transcripts = pyhgvs_utils.read_transcripts(infile)
 
+def get_transcript(name):
+    return transcripts.get(name)
+
+def VCF_to_HGVS(genome_coor):
+    pass
 
