67633a0dac6b585d3ab9b1c4080f11e0bf1cf007
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-13 10:33:12 -0700

    deal with cases where genomic coordinate is insufficient to represent a variant, such as structural variant

--- splicing_prior/extract_BRCAExchange_variants.py
+++ splicing_prior/extract_BRCAExchange_variants.py
@@ -4,18 +4,20 @@ import pdb
 
 import hgvs_conversion as hc
 
-FILE = "/hive/groups/cgl/brca/release1.0/pipeline_output/merged_01Jul2016.csv"
+FILE = "/hive/groups/cgl/brca/release1.0/pipeline_output/merged_13Jul2016.csv"
 SNP_VCF = "VCFSA_data/brcaExchange_SNPs.vcf"
 INDEL_VCF="VCFSA_data/brcaExchange_INDELs.vcf"
-
+OTHER="VCFSA_data/brcaExchange_others.txt"
 
 def main():
     write_header()
     f = open(FILE, "r")
     f_snp = open(SNP_VCF + ".body", "w")
     f_indel = open(INDEL_VCF + ".body", "w")    
+    f_other = open(OTHER, "w")
     n_snp = 0
     n_indel = 0
+    n_other = 0
     for index, line in enumerate(f):
         if index == 0:
             continue
@@ -26,23 +28,31 @@ def main():
         chr, pos, refalt = genome_coor.replace("-", "").split(":")
         ref, alt = refalt.split(">")
         chr = chr[-2:]
-        HGVS = hc.VCF_to_HGVS([chr, int(pos), ref, alt])
+        #HGVS = hc.VCF_to_HGVS([chr, int(pos), ref, alt])
+        HGVS = items[3] + ":" + items[4]
         info = "{0}".format(HGVS)
         if ref == "":
             ref = "-"
         if alt == "":
             alt = "-"
         new_line = "\t".join([chr, str(pos), ".", ref, alt, ".", ".", info]) + "\n"
-        if len(ref) == len(alt):
-            n_snp += 1
-            f_snp.write(new_line)
-        
+        if ref == "None" or alt == "None":
+            n_other += 1
+            f_other.write(line)
+        elif len(ref) == 1 and len(alt) == 1:
+            if ref != alt:
+                n_snp += 1
+                f_snp.write(new_line)
+            else:
+                n_other += 1
+                f_other.write(line)
         else:
             n_indel += 1
-            f_indel.write(new_line)
+            f_indel.write(new_line)    
     f.close()
     f_snp.close()
     f_indel.close()
+    f_other.close()
     for filename in [SNP_VCF, INDEL_VCF]:
         with open(filename, "w") as file:
             file_list = ["VCFSA_data/vcf_header.txt", filename + ".body"]
@@ -52,10 +62,10 @@ def main():
         os.remove(filename + ".body")
     print "number of SNPs: ", n_snp
     print "number of indels: ", n_indel
-
+    print "number of unprocessed cases: ", n_other
 
 def write_header(version="GRCh38"):
-    with open('vcf_header.txt', 'w') as f_header:
+    with open('VCFSA_data/vcf_header.txt', 'w') as f_header:
         f_header.write("##fileformat=VCFv4.0\n")
         f_header.write("##reference={0}\n".format(version))
         f_header.write("##INFO=<ID=HGVS,Number=.,Type=String,Description=\"HGVS representation of this variant\">\n")
