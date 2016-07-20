44e714244bbe9efe624ac7a82b015a06d24f9c65
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-20 14:09:03 -0700

    add brca no flank hg38 data

--- resources/genome_sequences/fetch_brca12_from_hg38.py
+++ resources/genome_sequences/fetch_brca12_from_hg38.py
@@ -2,12 +2,8 @@ import pysam
 
 chr13_fa = "/cluster/home/mollyzhang/reference_genome/hg38/chr13.fa"
 chr17_fa = "/cluster/home/mollyzhang/reference_genome/hg38/chr17.fa"
-BRCA1_START = 41196312
-BRCA2_START = 32889617
-BRCA1_FLANK = open("brca1_hg38.txt", "r").read() 
-BRCA2_FLANK = open("brca2_hg38.txt", "r").read() 
-BRCA1_NO_FLANK = open("brca1_hg38_no_flanking.txt", "r").read() 
-BRCA2_NO_FLANK = open("brca2_hg38_no_flanking.txt", "r").read() 
+BRCA1_NO_FLANK = "brca1_hg38_no_flanking.txt"
+BRCA2_NO_FLANK = "brca2_hg38_no_flanking.txt"
 
 
 def main():
@@ -17,11 +13,11 @@ def main():
 def fetch():
     chr13 = pysam.FastaFile(chr13_fa)
     chr17 = pysam.FastaFile(chr17_fa)
-    BRCA2 = chr13.fetch(reference="chr13", start=32889617, end=32973809)
-    BRCA1 = chr17.fetch(reference="chr17", start=41196312, end=41277500)
+    BRCA2 = chr13.fetch(reference="chr13", start=32315474, end=32400266)
+    BRCA1 = chr17.fetch(reference="chr17", start=43045629, end=43125483)
     
-    open(brca1, "w").write(BRCA1)
-    open(brca2, "w").write(BRCA2)
+    open(BRCA1_NO_FLANK, "w").write(BRCA1)
+    open(BRCA2_NO_FLANK, "w").write(BRCA2)
 
 if __name__=="__main__":
     main()
