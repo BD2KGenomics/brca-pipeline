d5675c5f56aad9ee63462ada89c262791fd10700
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-20 20:55:02 -0700

    add shell script to run MaxEntScan

---
+++ splicing_prior/MaxEntScan/run_MaxEntScan.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+perl score3.pl ../MES_data/BRCA1_3.txt > ../MES_data/BRCA1_3_score.txt
+perl score3.pl ../MES_data/BRCA2_3.txt > ../MES_data/BRCA2_3_score.txt
+perl score5.pl ../MES_data/BRCA1_5.txt > ../MES_data/BRCA1_5_score.txt
+perl score5.pl ../MES_data/BRCA2_5.txt > ../MES_data/BRCA2_5_score.txt
+
+
