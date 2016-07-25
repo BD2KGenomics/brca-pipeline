7df6c30e293adb8206bddc374d02f22220c49569
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-25 14:21:37 -0700

    update hgvs_conversion script to accomodate hg19 as well as hg38

--- splicing_prior/hgvs_conversion.py
+++ splicing_prior/hgvs_conversion.py
@@ -9,21 +9,29 @@ from pygr.seqdb import SequenceFileDB
 
 
 HG38 = SequenceFileDB("/cluster/home/mollyzhang/reference_genome/hg38/hg38.fa")
+HG19 = SequenceFileDB("/cluster/home/mollyzhang/reference_genome/hg19/hg19.fa")
+GENOME = {"hg38": HG38, "hg19": HG19}
+
 
 with open('../resources/refseq/hg38.BRCA.refGene.txt') as infile:
-    transcripts = hgvs_utils.read_transcripts(infile)
+    transcripts_hg38 = hgvs_utils.read_transcripts(infile)
+with open('../resources/refseq/hg19.BRCA.refGene.txt') as infile:
+    transcripts_hg19 = hgvs_utils.read_transcripts(infile)
+TRANSCRIPTS = {"hg19": transcripts_hg19, "hg38": transcripts_hg38}
 
-def get_transcript(name):
-    return transcripts.get(name)
 
-def VCF_to_HGVS(genome_coor):
+
+def get_transcript(name, version):
+    return TRANSCRIPTS[version].get(name)
+
+def VCF_to_HGVS(genome_coor, version="hg38"):
     chrom, pos, ref, alt = genome_coor
     if chrom == "chr13":
-        transcript = get_transcript("NM_000059")
+        transcript = get_transcript("NM_000059", version)
     elif chrom == "chr17":
-        transcript = get_transcript("NM_007294")
+        transcript = get_transcript("NM_007294", version)
     else:
         raise Exception("chromsome name error")
-    hgvs_name = pyhgvs.format_hgvs_name(chrom, pos, ref, alt, HG38, transcript)
+    hgvs_name = pyhgvs.format_hgvs_name(chrom, pos, ref, alt, GENOME[version], transcript)
     return hgvs_name
 
