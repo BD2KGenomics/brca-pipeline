0b8e52f7a398cf9bb2d5c81bf0241dec047b4d1f
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-27 16:04:57 -0700

    save positions as dictionaries of exons

--- splicing_prior/web_scraping/hci_website_scraping.py
+++ splicing_prior/web_scraping/hci_website_scraping.py
@@ -8,7 +8,7 @@ import urllib
 from bs4 import BeautifulSoup
 import sys 
 import glob
-
+import pprint
 
 BASE_URL = "http://priors.hci.utah.edu/PRIORS/BRCA/"
 URL_PREFIX = {"donor": BASE_URL + "popdon.php?", 
@@ -16,35 +16,54 @@ URL_PREFIX = {"donor": BASE_URL + "popdon.php?",
 
 
 def main():
-    get_all_HGVS_positions("BRCA1")
+    build_table()
+
 
 def get_all_HGVS_positions(gene):
     filenames = glob.glob("html_data/*{0}*".format(gene))
-    positions = set([])
+    exons = {} 
     for name in filenames:
         pos = name.split("?")[-1].split("&")[1].split("=")[-1]
-        positions.add(pos)
-    return positions
+        exon = name.split("?")[-1].split("&")[2].split("=")[-1]
+        if exon not in exons.keys():
+            exons[exon] = set([pos])
+        else:
+            exons[exon].add(pos)
+    return exons
 
 
 
 def build_table():
+    n = 0
+    for gene in ['BRCA1', 'BRCA2']:
+        exons = get_all_HGVS_positions(gene)
+        for exon in sorted(exons.keys()):
+            for pos in exons[exon]:
+                for mutation in ['1', '2', '3']:    
+                    for sj in ['donor', 'acceptor']:
+                        filename = "html_data/{0}gene={1}&pos={2}&exon={3}&mut={4}.html".format(
+                            URL_PREFIX[sj], gene, pos, exon, mutation)
+                        n += 1 
+        break
+    print n
+
+
 
     # acceptor
-    example = "html_data/popwtacc.php?gene=BRCA1&pos=999&exon=exon11&mut=3.html"
-    soup = BeautifulSoup(open(example, "r").read(), "html.parser")
-    wt = soup.findAll("td")[7]
-    mutant = soup.findAll("td")[11]
-    print wt
-    print mutant
-
-    # donor
-    example = "html_data/popdon.php?gene=BRCA1&pos=999&exon=exon11&mut=3.html"
-    soup = BeautifulSoup(open(example, "r").read(), "html.parser")
-    wt = soup.findAll("td")[8]
-    mutant = soup.findAll("td")[13]
-    print wt
-    print mutant
+#    example = "html_data/popwtacc.php?gene=BRCA1&pos=999&exon=exon11&mut=3.html"
+#    soup = BeautifulSoup(open(example, "r").read(), "html.parser")
+#    wt_score = soup.findAll("td")[7]
+#    mutant_score = soup.findAll("td")[11]
+#    print wt
+#    print mutant
+#
+#    # donor
+#    example = "html_data/popdon.php?gene=BRCA1&pos=999&exon=exon11&mut=3.html"
+#    soup = BeautifulSoup(open(example, "r").read(), "html.parser")
+#    wt = soup.findAll("td")[8]
+#    mutant = soup.findAll("td")[13]
+#    print wt
+#    print mutant
 
 
 def download():
