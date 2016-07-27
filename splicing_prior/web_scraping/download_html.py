59ba2a2af49ed7c3cecbca02d333c161a183fa00
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-27 13:57:24 -0700

    get URLs of all needed pages

--- splicing_prior/web_scraping/download_html.py
+++ splicing_prior/web_scraping/download_html.py
@@ -8,6 +8,10 @@ import urllib
 from bs4 import BeautifulSoup
 
 
+BASE_URL = "http://priors.hci.utah.edu/PRIORS/BRCA/"
+URL_PREFIX = {"donor": BASE_URL + "popdon.php?", 
+              "acceptor": BASE_URL + "popwtacc.php?"}
+
 
 def main():
     #download("http://priors.hci.utah.edu/PRIORS/BRCA/popdon.php?pos=1&gene=BRCA1&x=2&color=A9F5A9&promoted=no&exon=exon2&id=31&mut=3")
@@ -21,11 +25,29 @@ def download(address):
 
 def hgvs_positions():
     """get all the hgvs positions displayed by the priors website"""
+    base_url = "http://priors.hci.utah.edu/PRIORS/BRCA/viewer.php?gene={0}&exon={1}"
     for gene in ["BRCA1", "BRCA2"]:
-        url = "http://priors.hci.utah.edu/PRIORS/BRCA/viewer.php?gene={0}&exon=exon2".format(gene)
-        page = urllib.urlopen(url).read()
+        page = urllib.urlopen(base_url.format(gene, "exon2")).read()
         soup = BeautifulSoup(page, "html.parser")
-        print soup.prettify('utf-8')
+        last_exon = soup.findAll("td", {"class": 'exon'})[-1].a.contents[0]
+        for exon in range(2, int(last_exon)+1):
+            exon = str(exon)
+            print gene, exon
+            exon_url = base_url.format(gene, "exon" + exon)
+            exon_soup = BeautifulSoup(urllib.urlopen(exon_url).read(), "html.parser")
+            sequence = exon_soup.findAll("a", {"class": "seq"})
+            for base in sequence:
+                position = base.get('href').split("?")[1]
+                for splice_type, url_prefix in URL_PREFIX.iteritems():
+                    for mutation in ["&mut=1", "&mut=2", "&mut=3"]:
+                        this_url = url_prefix + position + mutation
+                        print this_url
+                    #    page = urllib.urlopen().read()
+                break
+ 
+            break
+        break
+
 
 
 if __name__ == "__main__":
