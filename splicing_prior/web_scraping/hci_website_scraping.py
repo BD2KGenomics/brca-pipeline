b7be9512cc088f952c76ed78cfeb119beeb7b382
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-28 20:40:35 -0700

    build table directly from website instead of downloading html

--- splicing_prior/web_scraping/hci_website_scraping.py
+++ splicing_prior/web_scraping/hci_website_scraping.py
@@ -7,88 +7,49 @@ from http://priors.hci.utah.edu/PRIORS/index.php
 import urllib
 from bs4 import BeautifulSoup
 import sys 
-import glob
 import pprint
 
 BASE_URL = "http://priors.hci.utah.edu/PRIORS/BRCA/"
-URL_PREFIX = {"donor": BASE_URL + "popdon.php?", 
-              "acceptor": BASE_URL + "popwtacc.php?"}
+
 
 
 def main():
-    build_table()
-
-
-def get_all_HGVS_positions(gene):
-    filenames = glob.glob("html_data/*{0}*".format(gene))
-    exons = {} 
-    for name in filenames:
-        pos = name.split("?")[-1].split("&")[1].split("=")[-1]
-        exon = name.split("?")[-1].split("&")[2].split("=")[-1]
-        if exon not in exons.keys():
-            exons[exon] = set([pos])
-        else:
-            exons[exon].add(pos)
-    return exons
-
-
-
-def build_table():
-    n = 0
-    for gene in ['BRCA1', 'BRCA2']:
-        exons = get_all_HGVS_positions(gene)
-        for exon in sorted(exons.keys()):
-            for pos in exons[exon]:
-                for mutation in ['1', '2', '3']:    
-                    for sj in ['donor', 'acceptor']:
-                        filename = "html_data/{0}gene={1}&pos={2}&exon={3}&mut={4}.html".format(
-                            URL_PREFIX[sj], gene, pos, exon, mutation)
-                        n += 1 
-        break
-    print n
-
-
-
-    # acceptor
-#    example = "html_data/popwtacc.php?gene=BRCA1&pos=999&exon=exon11&mut=3.html"
-#    soup = BeautifulSoup(open(example, "r").read(), "html.parser")
-#    wt_score = soup.findAll("td")[7]
-#    mutant_score = soup.findAll("td")[11]
-#    print wt
-#    print mutant
-#
-#    # donor
-#    example = "html_data/popdon.php?gene=BRCA1&pos=999&exon=exon11&mut=3.html"
-#    soup = BeautifulSoup(open(example, "r").read(), "html.parser")
-#    wt = soup.findAll("td")[8]
-#    mutant = soup.findAll("td")[13]
-#    print wt
-#    print mutant
+    download()
 
 
 def download():
     base_url = "http://priors.hci.utah.edu/PRIORS/BRCA/viewer.php?gene={0}&exon={1}"
     for gene in ["BRCA1", "BRCA2"]:
-        page = urllib.urlopen(base_url.format(gene, "exon2")).read()
-        soup = BeautifulSoup(page, "html.parser")
-        last_exon = soup.findAll("td", {"class": 'exon'})[-1].a.contents[0]
-        for exon in range(2, int(last_exon)+1):
-            exon = str(exon)
+        soup = BeautifulSoup(urllib.urlopen(base_url.format(gene, "exon2")).read(), "html.parser")
+        exon_tags = soup.findAll("td", {"class": "current-exon"}) + soup.findAll("td", {"class": 'exon'})
+        for exon_tag in exon_tags:
+            exon = exon_tag.a.contents[0]
             print "\n", gene, "exon{0}".format(exon)
+            f_out = open("scraped_data/{0}_exon{1}.txt".format(gene, exon), "w")
             exon_url = base_url.format(gene, "exon" + exon)
             exon_soup = BeautifulSoup(urllib.urlopen(exon_url).read(), "html.parser")
             sequence = exon_soup.findAll("a", {"class": "seq"})
             for base in sequence:
-                sys.stdout.write(".")
-                sys.stdout.flush()
                 position = base.get('href').split("?")[1]
-                for splice_type, url_prefix in URL_PREFIX.iteritems():
-                    for mutation in ["&mut=1", "&mut=2", "&mut=3"]:
-                        this_url = url_prefix + position + mutation
-                        filename = "html_data/" + this_url.split("/")[-1] + ".html"
-                        with open(filename, "w") as f:
-                            f.write(urllib.urlopen(this_url).read())
-                            f.close()
+                for mutation in ["&mut=1", "&mut=2", "&mut=3"]:
+                    create_line(f_out, position, mutation)
+                break
+            break
+            f_out.close()
+        break
+
+def create_line(f, pos, mut):
+    for splice_type in ["popdon?", "popwtacc?"]:
+        this_url = BASE_URL + splice_type + pos + mut
+        mut_soup = BeautifulSoup(urllib.urlopen(this_url).read(), "html.parser")
+        hgvs = mut_soup.span.div.contents[0].encode("ascii", "replace").split("?")[-1]
+        table_content = mut_soup.findAll("td")
+        if splice_type == "popdon?":
+            donor_score = table_content[13].div.contents[0]
+        elif splice_type == "popwtacc?":
+            acceptor_score = table_content[11].contents[0]
+    line = "\t".join([hgvs, donor_score, acceptor_score]) + "\n"
+    f.write(line)
 
 
 if __name__ == "__main__":
