aed232312b7aa6fa194eebe1acdbaaa1439745e5
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-07-28 23:13:30 -0700

    add script to compare SNP BRCA VCFSA result with hci priors website result

---
+++ splicing_prior/confirm_SNP_VSFSA_result.py
@@ -0,0 +1,14 @@
+"""
+compared the all possible SNP brca variants VCFSA result with the scraped data from the 
+hci BRCA priors website and calculate consistency between the two
+"""
+
+
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
