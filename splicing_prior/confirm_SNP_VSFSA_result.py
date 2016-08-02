eada74962f3ee5149a4e393255f6286f2fe55672
Can (Molly) Zhang <mollyzhang@ucsc.edu>
2016-08-02 11:10:46 -0700

    add comparison of MES scores between hci website and molly's vcfsa run

--- splicing_prior/confirm_SNP_VSFSA_result.py
+++ splicing_prior/confirm_SNP_VSFSA_result.py
@@ -12,17 +12,44 @@ HCI_WEBSITE = "/cluster/home/mollyzhang/brca-pipeline/splicing_prior/web_scrapin
 
 
 def main():
-    print len(get_Molly_result_as_dict())
+    molly_result = get_Molly_result_as_dict()
+    compare_results(molly_result, HCI_WEBSITE)    
+
+
+def compare_results(result1, filename):
+    overlap = 0
+    donor_match = 0
+    f = open(filename, "r")
+    for index, line in enumerate(f):
+        if index == 0:
+            continue
+        if index %1000 == 0:
+            print index
+        items = line.strip().split("\t")
+        HGVS = "({0}):{1}".format(items[3], items[0].strip()).upper()
+        if HGVS in result1:
+            overlap += 1
+            donor = items[1]
+            print HGVS
+            print donor
+            print result1[HGVS]
+            if donor == result1[HGVS]["donor"]:
+                donor_match += 1 
+
+    percent_overlap = (overlap/(float(index) + 1)) * 100
+    print "%.2f percent hci website results can be found in Molly's result" %(percent_overlap)
+    print donor_match
 
 
 def get_Molly_result_as_dict():
+    print "save Molly's result to dictionary....."
     result = {}
     f = open(MOLLY_RESULT, "r")
     for index, line in enumerate(f):
         if line.startswith("#") or "c.*" in line:
             continue
         info = line.strip().split("\t")[-1]
-        HGVS = info.split(";")[0][5:]
+        HGVS = "(" + info.split(";")[0][5:].split("(")[-1].upper()
         position = extract_intron_position(HGVS)
         if position >= 26:
             continue
@@ -33,14 +60,20 @@ def get_Molly_result_as_dict():
             scores = {}
             for sj in MES.split(":"):
                 type = sj.split(",")[0][1]
+                if type == "3":
+                    type = "acceptor"
+                elif type == "5":
+                    type = "donor"
+                else:
+                    raise Exception("bad time")
                 score = sj.split(",")[-1]
-                scores[type] = score            
+                scores[type] = score
         result[HGVS] = scores
     return result
 
 
 def extract_intron_position(HGVS):
-    new_HGVS = HGVS.replace("c.-", "haha").replace("+", "-")
+    new_HGVS = HGVS.replace("C.-", "haha").replace("+", "-")
     if "-" not in new_HGVS:
         pos = 0
     else:
